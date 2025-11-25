4ac5621432bc42fd831f79a43c264a4e0d1b0d49
erik-cahya <erik.cahya841@gmail.com>
2025-11-26 01:00:08 +0800

    first commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,23 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'http',
+                hostname: 'http://localhost:8000',
+                port: '8000',
+                pathname: '/api/**',
+            },
+        ],
+    },
+    webpack: (config) => {
+        config.module.rules.push({
+            test: /\.svg$/,
+            use: ["@svgr/webpack"],
+        });
+
+        return config;
+    },
+};
 
 export default nextConfig;
