62eba6bef83e66373f80365687694810b5a4c159
erik-cahya <erik.cahya841@gmail.com>
2025-11-26 01:13:06 +0800

    setup project

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,8 +8,10 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
+  modal
 }: Readonly<{
   children: React.ReactNode;
+  modal: React.ReactNode;
 }>) {
   return (
     <html lang="en">
@@ -22,7 +24,11 @@ export default function RootLayout({
       </head>
 
       <body>
-        {children}</body>
+
+        {children}
+        {modal}
+
+      </body>
     </html>
   );
 }
--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,5 +1,23 @@
 "use client"
 
-export function PreventScrolling() {
+import { useRouter } from "next/navigation";
+import { useLayoutEffect } from "react"
 
+
+export function PreventScrolling() {
+    useLayoutEffect(() => {
+        document.querySelector('body')!.classList.add("overflow-hidden")
+
+        return () => {
+            document.querySelector('body')!.classList.remove("overflow-hidden")
+        };
+    }, []);
+
+    return null
+}
+
+export function RouterBack() {
+
+    const router = useRouter();
+    return <div className="absolute inset-0 z-10 cursor-pointer" onClick={router.back}></div>
 }
