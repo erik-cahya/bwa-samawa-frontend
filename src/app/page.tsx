cc9f462344db66c5647817c44159672d98068efe
erik-cahya <erik.cahya841@gmail.com>
2025-11-27 02:35:30 +0800

    update : Wedding Package Slider

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,11 @@
 import Header from "@/components/Header";
+import PropsWeddingPakcageWrapper from "@/components/WeddingPakcages";
+
 
 export default function Home() {
   return (
     <main className="flex flex-col gap-y-16">
+      <PropsWeddingPakcageWrapper show="popular" type="slider" />
       <Header />
       test
     </main>
---
+++ src/components/WeddingPakcages/types.d.ts
@@ -0,0 +1,13 @@
+export type TShow = "popular" | "newest";
+
+type TPackage = {
+    id: number,
+    name: string,
+    slug: string,
+    price: number,
+    isPopular: 1 | 0,
+    thumbnail: string,
+    about: string,
+    city: TCity,
+    weddingOrganizer: TOrganizer
+}
